d7964ec6e4be2a93199e3a9192bcbce9d82677be
Colin Dean <colindean@users.noreply.github.com>
2023-08-04 16:41:03 -0400

    Use more robust importlib metadata importer from build
    Reference: https://github.com/pypa/build/blob/381ce789e1662e2e16b0ca4be520cdcf7464f5a3/src/build/_importlib.py#L5

--- src/pygccxml/__init__.py
+++ src/pygccxml/__init__.py
@@ -42,10 +42,15 @@ warnings.simplefilter("always", DeprecationWarning)
 # TODO:
 # 1. Add "explicit" property for constructors
 
-if sys.version_info[:2] >= (3, 8):
-    from importlib import metadata
-else:
+if sys.version_info < (3, 8):
     import importlib_metadata as metadata
+elif sys.version_info < (3, 9, 10) or (3, 10, 0) <= sys.version_info < (3, 10, 2):
+    try:
+        import importlib_metadata as metadata
+    except ModuleNotFoundError:
+        from importlib import metadata
+else:
+    from importlib import metadata
 
 __version__ = metadata.version("pygccxml")
 
